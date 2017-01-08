1a2c99370057863d8cfab1bf94b3fd4bcc9f27b2
Muhamad Alfan <muhamad.alfan01@gmail.com>
2017-01-08 17:49:48 +0700

    panduan instalasi dan penggunaan

--- config/params.php
+++ config/params.php
@@ -3,7 +3,7 @@
 return [
     'adminEmail'   => 'admin@example.com',
 
-    'urlApi'       => 'http://alfan.dev/test-programmer/server-side/api/public_html/v1/',
+    'urlApi'       => 'http://api.muhamadalfan.com/v1/',
 
     'tokenDefault' => "Z2V0VG9rZW5Gcm9tQWNjZXNzTG9naW4=",
 ];
